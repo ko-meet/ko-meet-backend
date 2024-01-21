a18ca5dfe7d82e21e9280651404c16c2b6bd9771
HyunsooZo <bzhs1992@icloud.com>
2024-01-21 23:56:13 +0900

    feat: 리프레시 토큰 관련 로직 제거

--- src/test/java/com/backend/komeet/service/user/service/user/UserSignUpServiceTest.java
+++ src/test/java/com/backend/komeet/service/user/service/user/UserSignUpServiceTest.java
@@ -40,7 +40,7 @@ public class UserSignUpServiceTest {
     UserSignUpRequest request = UserSignUpRequest.builder()
             .email("test@test.com")
             .nickName("test")
-            .country(Countries.SOUTH_KOREA)
+            .country("SOUTH_KOREA")
             .password("test")
             .build();
 
