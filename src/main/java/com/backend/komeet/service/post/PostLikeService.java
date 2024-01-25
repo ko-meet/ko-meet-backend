bde7a81c4a6c61fef1780c33c1576a4d5c0d27fa
HyunsooZo <bzhs1992@icloud.com>
2024-01-25 21:40:21 +0900

    feat: 게시물 좋아요 기능 추가

--- src/main/java/com/backend/komeet/service/post/PostLikeService.java
+++ src/main/java/com/backend/komeet/service/post/PostLikeService.java
@@ -9,13 +9,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -32,18 +25,15 @@ public class PostLikeService {
      * @param postSeq 게시물 식별자
      */
     @Async
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public Long likePost(Long userSeq, Long postSeq) {
+    public void likePost(Long userSeq, Long postSeq) {
         int maxRetries = 3;
         int retries = 0;
         long retryIntervalMillis = 1000; // 1초 간격으로 재시도
-        Long likeCount = null;
 
         while (retries <= maxRetries) {
             try {
-                likeCount = processLike(userSeq, postSeq);
-                if (likeCount != null) {
-                    break;
+                if (processLike(userSeq, postSeq)) {
+                    return;
                 } else {
                     log.error("Failed to acquire lock for postSeq: {}", postSeq);
                 }
@@ -59,34 +49,6 @@ public class PostLikeService {
                 }
             }
         }
-        // 4번까지 시도하여 값을 받을 때까지 대기
-        int maxAttempts = 4;
-        int attempts = 0;
-        long waitIntervalMillis = 1000; // 1초 간격으로 재시도
-        while (attempts < maxAttempts) {
-            try {
-                // CompletableFuture를 통해 작업이 완료될 때까지 대기
-                CompletableFuture<Long> future = CompletableFuture.completedFuture(likeCount);
-                likeCount = future.get(1, TimeUnit.SECONDS);
-                if (likeCount != null) {
-                    break;
-                }
-            } catch (TimeoutException | InterruptedException | ExecutionException e) {
-                log.error("Failed to get likeCount for postSeq: {}", postSeq);
-            }
-            attempts++;
-            try {
-                Thread.sleep(waitIntervalMillis);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-
-        if (likeCount == null) {
-            throw new RuntimeException("작업이 완료되지 않았습니다.");
-        }
-
-        return likeCount;
     }
 
     /**
@@ -97,14 +59,12 @@ public class PostLikeService {
      * @return 작업 성공 여부
      * @throws CustomException 작업 실패
      */
-    private Long processLike(Long userSeq, Long postSeq) throws CustomException {
+    private boolean processLike(Long userSeq, Long postSeq) throws CustomException {
         Post post = getPost(postSeq);
 
         boolean lockAcquired = false;
         try {
-            lockAcquired = redisDistributedLock.acquireLock(
-                    LOCK_KEY, postSeq.toString(), 10
-            );
+            lockAcquired = redisDistributedLock.acquireLock(LOCK_KEY, postSeq.toString(), 10);
             if (lockAcquired) {
                 if (post.getLikeUsers().remove(userSeq)) {
                     post.setLikeCount(post.getLikeCount() - 1);
@@ -112,9 +72,9 @@ public class PostLikeService {
                     post.getLikeUsers().add(userSeq);
                     post.setLikeCount(post.getLikeCount() + 1);
                 }
-                return postRepository.save(post).getLikeCount();
+                return true;
             } else {
-                return null;
+                return false;
             }
         } finally {
             if (lockAcquired) {
