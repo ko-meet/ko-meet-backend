fa209fb114f1b3ae8df2ee329b93730397166c83
HyunsooZo <bzhs1992@icloud.com>
2024-02-03 04:58:05 +0900

    merge: 도커 파일 수정 (#19)
    * feat: 게시물 등록 api 추가
    
    * rename: 서비스 및 메서드명 리네이밍
    
    * feat: 이미지 업로드 로직 분리
    
    * feat: 리프레시 토큰 관련 로직 제거
    
    * feat: 게시물 좋아요 기능 추가
    
    * feat: 게시물 좋아요 기능 반환값 추가
    
    * feat: 게시물 전체 조회 기능 추가
    
    * feat: 게시물 전체 조회 기능 조건 수정
    
    * feat: 게시물 상세 조회 기능 추가
    
    * merge: Dockerfile 수정

--- src/test/java/com/backend/komeet/service/user/KoMeetApplicationTests.java
+++ src/test/java/com/backend/komeet/service/user/KoMeetApplicationTests.java
@@ -1,13 +1,13 @@
-package com.backend.komeet.service.user;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class KoMeetApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.backend.komeet.service.user;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class KoMeetApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
