b29bc45f2c62f58b2945e6ece7a774e2fae3cacb
HyunsooZo <bzhs1992@icloud.com>
2024-03-03 04:21:04 +0900

    fix: 프로필 이미지 버그 수정

--- src/main/java/com/backend/komeet/service/user/UserInformationService.java
+++ src/main/java/com/backend/komeet/service/user/UserInformationService.java
@@ -58,6 +58,8 @@ public class UserInformationService {
                     CountryUtil.fetchLocation(country);
             user.setRegion(countryPair.getSecond());
             user.setCountry(userInfoUpdateRequest.getCountry());
+        }
+        if (userInfoUpdateRequest.getProfileImage() != null) {
             user.setImageUrl(userInfoUpdateRequest.getProfileImage());
         }
     }
@@ -128,7 +130,7 @@ public class UserInformationService {
 
         Pair<String, String> countryPair = CountryUtil.fetchLocation(country);
 
-        if(user.getCountry().getCountryName().equals(countryPair.getFirst()) &&
+        if (user.getCountry().getCountryName().equals(countryPair.getFirst()) &&
                 user.getRegion().equals(countryPair.getSecond())) {
             isLocationMatch = true;
         }
@@ -141,7 +143,7 @@ public class UserInformationService {
         );
 
         return UserSignInDto.from(
-            user,accessToken,refreshToken,isLocationMatch
+                user, accessToken, refreshToken, isLocationMatch
         );
     }
 
@@ -151,7 +153,7 @@ public class UserInformationService {
      * @param token 토큰
      * @return 토큰
      */
-    public Pair<String,String> refreshToken(String token) {
+    public Pair<String, String> refreshToken(String token) {
         String userEmail = redisService.getValueByKey(TOKEN_PREFIX + token);
         if (userEmail == null) {
             throw new CustomException(USER_INFO_NOT_FOUND);
