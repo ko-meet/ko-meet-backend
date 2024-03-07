196add04e677a5165fa2cf940f52b86b896d1d54
HyunsooZo <bzhs1992@icloud.com>
2024-03-07 22:08:01 +0900

    merge: 포스팅 검색 기능 개선 (잘린 문자 "..."처리) (#85)
    * feat: 채팅방 목록 업데이트 기능 수정
    
    * feat: 포스팅 검색 기능 개선
    
    노출 컨텐트 길이를 늘리고, 잘린부분이 있을경우 "..." 삽입

--- src/main/java/com/backend/komeet/service/post/PostUploadService.java
+++ src/main/java/com/backend/komeet/service/post/PostUploadService.java
@@ -84,7 +84,7 @@ public class PostUploadService {
      * 게시물 검색 메서드
      *
      * @param keyword 검색어
-     * @param page
+     * @param page 페이지
      * @return 검색된 게시물
      */
     @Transactional(readOnly = true)
