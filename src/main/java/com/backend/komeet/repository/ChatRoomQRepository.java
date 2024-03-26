c82deef402c7e1de62661d201df1e85e41816cf6
HyunsooZo <bzhs1992@icloud.com>
2024-03-26 21:00:14 +0900

    feat: 채팅목록 검색 페이징 제거

--- src/main/java/com/backend/komeet/repository/ChatRoomQRepository.java
+++ src/main/java/com/backend/komeet/repository/ChatRoomQRepository.java
@@ -5,6 +5,7 @@ import com.backend.komeet.dto.ChatRoomDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -16,8 +17,7 @@ public interface ChatRoomQRepository {
 
     Optional<ChatRoomDto> getChatRoom(User user1, User user2);
 
-    Page<ChatRoomDto> searchChatRoomsByUserNickName(User user,
-                                                    String keyword,
-                                                    Pageable pageable);
+    List<ChatRoomDto> searchChatRoomsByUserNickName(User user,
+                                                    String keyword);
 
 }
