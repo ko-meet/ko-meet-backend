e8cf0775930aff75d1033d8999324d27a7c943ca
HyunsooZo <bzhs1992@icloud.com>
2024-05-13 23:44:33 +0900

    test: 테스트코드 경로 수정 및 리팩토링

--- src/test/java/com/backend/komeet/service/post/PostUpdateServiceTest.java
+++ src/test/java/com/backend/komeet/service/post/PostUpdateServiceTest.java
@@ -1,11 +1,12 @@
 package com.backend.komeet.service.post;
 
-import com.backend.komeet.post.model.entities.Post;
-import com.backend.komeet.user.model.entities.User;
 import com.backend.komeet.post.application.PostUpdateService;
+import com.backend.komeet.post.model.entities.Post;
 import com.backend.komeet.post.presentation.request.PostUpdateRequest;
-import com.backend.komeet.user.enums.Countries;
 import com.backend.komeet.post.repositories.PostRepository;
+import com.backend.komeet.service.common.TestEntityGenerator;
+import com.backend.komeet.service.common.TestRequestGenerator;
+import com.backend.komeet.user.model.entities.User;
 import com.backend.komeet.user.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,7 +14,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -36,34 +36,15 @@ class PostUpdateServiceTest {
         postUpdateService = new PostUpdateService(postRepository, userRepository);
     }
 
-    User user = User.builder()
-            .email("test@test.test")
-            .password("test")
-            .country(Countries.SOUTH_KOREA)
-            .build();
-    Post post = Post.builder()
-            .seq(1L)
-            .title("제목")
-            .attachments(List.of("첨부파일1", "첨부파일2"))
-            .tags(List.of("태그1", "태그2"))
-            .isPublic("Y")
-            .country(Countries.LAOS)
-            .region("지역")
-            .user(user)
-            .content("내용")
-            .comments(List.of())
-            .build();
+    User user = TestEntityGenerator.user;
+    Post post = TestEntityGenerator.postNormal;
+    PostUpdateRequest postUpdateRequest =
+            TestRequestGenerator.createPostUpdateRequest();
 
     @Test
     @DisplayName("성공")
     void updatePost() {
         // given
-        PostUpdateRequest postUpdateRequest = PostUpdateRequest.builder()
-                .title("새제목")
-                .content("새내용")
-                .isPublic(false)
-                .build();
-
         when(userRepository.findById(1L)).thenReturn(Optional.of(user));
         when(postRepository.findById(1L)).thenReturn(Optional.of(post));
         // when
