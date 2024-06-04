60d89a9ff8c4ed89fcc10d6d4378bdd3a6cdf91e
HyunsooZo <bzhs1992@icloud.com>
2024-06-04 20:59:49 +0900

    merge: 공지사항 상세조회 , 수정, 삭제 기능 추가 (#132)
    * feat: 공지사항 기능 개선
    
    * feat: 공지사항 목록 조회기능 추가
    
    * feat: 공지사항 상세조회 기능 추가
    
    * feat: 공지사항 수정(및 삭제) 기능 추가

--- src/main/java/com/backend/komeet/notice/presentation/controller/NoticeController.java
+++ src/main/java/com/backend/komeet/notice/presentation/controller/NoticeController.java
@@ -5,6 +5,7 @@ import com.backend.komeet.infrastructure.security.JwtProvider;
 import com.backend.komeet.notice.application.NoticeInquiryService;
 import com.backend.komeet.notice.application.NoticeModifyService;
 import com.backend.komeet.notice.application.NoticeRegisterService;
+import com.backend.komeet.notice.presentation.request.NoticeModifyRequest;
 import com.backend.komeet.notice.presentation.request.NoticeRegisterRequest;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
 
 /**
@@ -48,6 +50,28 @@ public class NoticeController {
                 .body(new ApiResponse(
                         noticeInquiryService.getNotices(userSeq, page)
                 ));
+    }
+
+    @GetMapping("/{noticeSeq}")
+    @ApiOperation(value = "공지사항 상세 조회", notes = "공지사항을 상세 조회합니다.")
+    public ResponseEntity<ApiResponse> getNotice(@RequestHeader String token,
+                                                 @PathVariable Long noticeSeq) {
+        Long userSeq = jwtProvider.getIdFromToken(token);
+        return ResponseEntity
+                .status(OK)
+                .body(new ApiResponse(
+                        noticeInquiryService.getNotice(userSeq, noticeSeq)
+                ));
+    }
+
+    @PatchMapping("/{noticeSeq}")
+    @ApiOperation(value = "공지사항 수정", notes = "공지사항을 수정합니다.")
+    public ResponseEntity<Void> modifyNotice(@RequestHeader String token,
+                                             @PathVariable Long noticeSeq,
+                                             @RequestBody NoticeModifyRequest request) {
+        Long userSeq = jwtProvider.getIdFromToken(token);
+        noticeModifyService.modifyNotice(userSeq, noticeSeq, request);
+        return ResponseEntity.status(NO_CONTENT).build();
     }
 
 }
