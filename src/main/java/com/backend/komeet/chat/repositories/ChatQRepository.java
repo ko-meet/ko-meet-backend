61ddf865942df62c5a6ab1085ed31d5c7587dcae
HyunsooZo <bzhs1992@icloud.com>
2024-06-11 19:49:11 +0900

    merge: 사용자에게 읽지않은 메세지가 있는지 확인하는 기능 추가 (#142)
    * chore: log 파일 생성 위치 수정
    
    * feat: 사용자에게 읽지 않은 메세지가 있는지 확인하는 기능 추가

--- src/main/java/com/backend/komeet/chat/repositories/ChatQRepository.java
+++ src/main/java/com/backend/komeet/chat/repositories/ChatQRepository.java
@@ -1,6 +1,7 @@
 package com.backend.komeet.chat.repositories;
 
 import com.backend.komeet.chat.model.dtos.ChatDto;
+import com.backend.komeet.user.model.entities.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -9,4 +10,5 @@ import org.springframework.data.domain.Pageable;
  */
 public interface ChatQRepository {
     Page<ChatDto> getChats(Long counterpartSeq, Long userSeq, Pageable pageable);
+    boolean hasUnreadMessages(Long userSeq);
 }
