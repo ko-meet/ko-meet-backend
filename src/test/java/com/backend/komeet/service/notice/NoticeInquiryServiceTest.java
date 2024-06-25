ffd68dd54c2e3a51e6ff438a50c62beec04256cd
hyunsooZo <bzhs1992@icloud.com>
2024-06-25 13:33:03 +0900

    test: 읽지 않은 공지사항 존재 여부 체크 테스트 코드 추가

--- src/test/java/com/backend/komeet/service/notice/NoticeInquiryServiceTest.java
+++ src/test/java/com/backend/komeet/service/notice/NoticeInquiryServiceTest.java
@@ -17,8 +17,7 @@ import org.springframework.data.domain.PageRequest;
 import java.util.List;
 import java.util.Optional;
 
-import static com.backend.komeet.service.common.TestEntityGenerator.notice;
-import static com.backend.komeet.service.common.TestEntityGenerator.user;
+import static com.backend.komeet.service.common.TestEntityGenerator.*;
 import static org.mockito.Mockito.when;
 
 @DisplayName("공지사항 조회 테스트")
@@ -59,5 +58,30 @@ class NoticeInquiryServiceTest {
             }
             Assertions.assertThat(noticeDto.getContent()).isEqualTo(content + "...");
         });
+    }
+
+    @Test
+    @DisplayName("안 앍은 공지사항 존재여부 체크 - 존재/미존재")
+    void checkUnreadNotice_Success() {
+        // given
+        Long userSeq1 = 1L;
+        Long userSeq2 = 2L;
+        when(userRepository.findById(userSeq1)).thenReturn(Optional.of(user));
+        when(userRepository.findById(userSeq2)).thenReturn(Optional.of(admin));
+        when(noticeRepository.existsByTargetCountriesContainingAndReadUsersNotContaining(
+                user.getCountry(), user.getSeq()
+        )).thenReturn(true);
+        when(noticeRepository.existsByTargetCountriesContainingAndReadUsersNotContaining(
+                admin.getCountry(), admin.getSeq()
+        )).thenReturn(true);
+
+        // when
+        boolean result1 = noticeInquiryService.isNoticeExist(userSeq1);
+        boolean result2 = noticeInquiryService.isNoticeExist(userSeq2);
+
+        // then
+        Assertions.assertThat(result1).isTrue();
+        Assertions.assertThat(result2).isTrue();
+
     }
 }
