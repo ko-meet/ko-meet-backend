e4836a95945056c10e1109d8b3aae018c93834fd
HyunsooZo <bzhs1992@icloud.com>
2024-07-08 20:13:18 +0900

    fix: 국가 정보 한글 이슈 수정 (#163)
    * feat: 국가 이름 한글로 넘어와도 처리가능하도록 수정
    
    * fix: 국가 정보 한글 이슈 수정

--- src/main/java/com/backend/komeet/user/response/LocationResponse.java
+++ src/main/java/com/backend/komeet/user/response/LocationResponse.java
@@ -1,5 +1,6 @@
 package com.backend.komeet.user.response;
 
+import com.backend.komeet.user.enums.Countries;
 import lombok.*;
 
 @Getter
@@ -10,9 +11,9 @@ public class LocationResponse {
     private String country;
     private String region;
 
-    public static LocationResponse from(String country, String region) {
+    public static LocationResponse from(Countries country, String region) {
         return LocationResponse.builder()
-                .country(country)
+                .country(country.getCountryName())
                 .region(region)
                 .build();
     }
