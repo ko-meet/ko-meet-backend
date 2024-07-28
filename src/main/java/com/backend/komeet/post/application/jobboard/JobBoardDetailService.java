c492f2d7969c8029f9ef7c879ff946e75544f603
HyunsooZo <bzhs1992@icloud.com>
2024-07-28 20:20:27 +0900

    feat: 구인구직 게시판 업로드 로직 수정
    - 기존 회사정보를 모두 입력받던 구조에서
      등록한 회사정보를 통해 등록하는 구조로 수정

--- src/main/java/com/backend/komeet/post/application/jobboard/JobBoardDetailService.java
+++ src/main/java/com/backend/komeet/post/application/jobboard/JobBoardDetailService.java
@@ -3,7 +3,6 @@ package com.backend.komeet.post.application.jobboard;
 import com.backend.komeet.infrastructure.exception.CustomException;
 import com.backend.komeet.post.model.dtos.JobBoardDto;
 import com.backend.komeet.post.repositories.JobBoardRepository;
-import com.backend.komeet.user.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,15 +20,14 @@ public class JobBoardDetailService {
 
     /**
      * 구인 게시판 상세 정보를 조회하는 메서드
-     *
-     * @param jobBoardSeq 구인 게시판 식별자
-     * @return {@link JobBoardDto}
      */
-    public JobBoardDto getJobBoardDetail(Long jobBoardSeq) {
+    public JobBoardDto getJobBoardDetail(
+            Long jobBoardSeq
+    ) {
         return JobBoardDto.from(
-                jobBoardRepository.findById(jobBoardSeq).orElseThrow(
-                        () -> new CustomException(JOB_BOARD_NOT_FOUND)
-                )
+                jobBoardRepository
+                        .findById(jobBoardSeq)
+                        .orElseThrow(() -> new CustomException(JOB_BOARD_NOT_FOUND))
         );
     }
 }
--- src/main/java/com/backend/komeet/post/application/jobboard/JobBoardUploadService.java
+++ src/main/java/com/backend/komeet/post/application/jobboard/JobBoardUploadService.java
@@ -1,17 +1,20 @@
 package com.backend.komeet.post.application.jobboard;
 
-import com.backend.komeet.post.model.entities.JobBoard;
-import com.backend.komeet.user.model.entities.User;
-import com.backend.komeet.post.model.dtos.JobBoardDto;
-import com.backend.komeet.post.presentation.request.JobBoardUploadRequest;
+import com.backend.komeet.company.model.entities.Company;
+import com.backend.komeet.company.repositories.CompanyRepository;
 import com.backend.komeet.infrastructure.exception.CustomException;
+import com.backend.komeet.post.model.dtos.JobBoardDto;
+import com.backend.komeet.post.model.entities.JobBoard;
+import com.backend.komeet.post.presentation.request.JobBoardUploadRequest;
 import com.backend.komeet.post.repositories.JobBoardRepository;
+import com.backend.komeet.user.model.entities.User;
 import com.backend.komeet.user.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.backend.komeet.infrastructure.exception.ErrorCode.COMPANY_NOT_FOUND;
 import static com.backend.komeet.infrastructure.exception.ErrorCode.USER_INFO_NOT_FOUND;
 
 /**
@@ -23,41 +26,47 @@ import static com.backend.komeet.infrastructure.exception.ErrorCode.USER_INFO_NO
 public class JobBoardUploadService {
     private final JobBoardRepository jobBoardRepository;
     private final UserRepository userRepository;
+    private final CompanyRepository companyRepository;
 
     /**
      * 구인구직 게시글을 업로드하는 메서드
-     *
-     * @param jobBoardRequest {@link JobBoardUploadRequest}
-     * @param userSeq         사용자 식별자
-     * @return {@link JobBoardDto}
      */
     @Transactional
-    public JobBoardDto postJobBoard(JobBoardUploadRequest jobBoardRequest,
-                                    Long userSeq) {
+    public JobBoardDto postJobBoard(
+            JobBoardUploadRequest jobBoardRequest,
+            Long userSeq
+    ) {
         User user = getUser(userSeq);
         return JobBoardDto.from(getJobBoard(jobBoardRequest, user));
     }
 
     /**
      * 구인구직 게시글 엔티티를 생성하는 메서드
-     *
-     * @param jobBoardRequest {@link JobBoardUploadRequest}
-     * @param user            {@link User}
-     * @return {@link JobBoard}
      */
-    private JobBoard getJobBoard(JobBoardUploadRequest jobBoardRequest, User user) {
-        return jobBoardRepository.save(JobBoard.from(jobBoardRequest, user));
+    private JobBoard getJobBoard(
+            JobBoardUploadRequest jobBoardRequest,
+            User user
+    ) {
+        Company company = getCompany(jobBoardRequest.getCompanySeq());
+        return jobBoardRepository.save(JobBoard.from(jobBoardRequest, company, user));
     }
 
     /**
      * 사용자 엔티티를 조회하는 메서드
-     *
-     * @param userSeq 사용자 식별자
-     * @return {@link User}
      */
-    private User getUser(Long userSeq) {
+    private User getUser(
+            Long userSeq
+    ) {
         return userRepository
                 .findById(userSeq)
                 .orElseThrow(() -> new CustomException(USER_INFO_NOT_FOUND));
+    }
+
+    private Company getCompany(
+            Long companySeq
+    ) {
+        return companyRepository
+                .findById(companySeq)
+                .orElseThrow(() -> new CustomException(COMPANY_NOT_FOUND));
     }
 }
