1a9f736ad915d6d6eac80ca0dab23a864a484dde
HyunsooZo <bzhs1992@icloud.com>
2024-08-10 06:30:19 +0900

    refactor: bookmark 엔티티 리팩토링

--- src/main/java/com/backend/komeet/post/model/entities/Bookmark.java
+++ src/main/java/com/backend/komeet/post/model/entities/Bookmark.java
@@ -1,12 +1,16 @@
 package com.backend.komeet.post.model.entities;
 
 import com.backend.komeet.base.model.entities.BaseEntity;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicUpdate;
 
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @AllArgsConstructor
@@ -20,15 +24,6 @@ public class Bookmark extends BaseEntity {
     private Long bookmarkSeq;
     private Long userSeq;
 
-//    @ManyToMany(fetch = FetchType.LAZY)
-//    @JoinTable(
-//            name = "bookmark_post",
-//            joinColumns = @JoinColumn(name = "bookmark_seq"),
-//            inverseJoinColumns = @JoinColumn(name = "post_seq")
-//    )
-//    @OrderBy("createdAt DESC")
-//    private List<Post> bookmarkPosts = new ArrayList<>();
-
     /**
      * 사용자 seq로 북마크 객체 생성
      */
@@ -37,18 +32,5 @@ public class Bookmark extends BaseEntity {
                 .userSeq(userSeq)
                 .build();
     }
-//
-//    /**
-//     * 북마크에 게시물 추가
-//     */
-//    public void addPost(Post post) {
-//        bookmarkPosts.add(post);
-//    }
-//
-//    /**
-//     * 북마크에서 게시물 삭제
-//     */
-//    public void removePost(Post post) {
-//        bookmarkPosts.remove(post);
-//    }
+
 }
