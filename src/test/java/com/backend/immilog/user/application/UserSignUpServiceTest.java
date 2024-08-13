18829ff0c83138a4c3d53f3b8c3fcf973d451e15
HyunsooZo <bzhs1992@icloud.com>
2024-08-13 22:33:07 +0900

    feat: 사용자 회원가입 및 인증메일 전송 기능 추가 (#195)
    * test (sign-up) : 회원가입 테스트 코드 추가
    
    * feat (sign-up) : 회원가입 및 인증메일 전송 기능 추가

---
+++ src/test/java/com/backend/immilog/user/application/UserSignUpServiceTest.java
@@ -0,0 +1,81 @@
+package com.backend.immilog.user.application;
+
+import com.backend.immilog.global.exception.CustomException;
+import com.backend.immilog.user.infrastructure.UserRepository;
+import com.backend.immilog.user.model.entities.User;
+import com.backend.immilog.user.presentation.request.UserSignUpRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.util.Pair;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static com.backend.immilog.global.exception.ErrorCode.EXISTING_USER;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+@DisplayName("사용자 회원가입 서비스 테스트")
+class UserSignUpServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    private UserSignUpService userSignUpService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        userSignUpService = new UserSignUpService(userRepository, passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("회원가입 - 성공")
+    void signUp_success() {
+        // given
+        UserSignUpRequest param = UserSignUpRequest.builder()
+                .nickName("test")
+                .password("test1234")
+                .email("email@email.com")
+                .country("SOUTH_KOREA")
+                .interestCountry("SOUTH_KOREA")
+                .region("Seoul")
+                .profileImage("image")
+                .build();
+
+        User userEntity = mock(User.class);
+        when(userEntity.getSeq()).thenReturn(1L);
+        when(userEntity.getNickName()).thenReturn("test");
+        when(userRepository.save(any(User.class))).thenReturn(userEntity);
+        when(passwordEncoder.encode(anyString())).thenReturn("test1234");
+        Pair<Long, String> seqAndNickName = userSignUpService.signUp(param);
+        // then
+        assertThat(seqAndNickName.getSecond()).isEqualTo("test");
+    }
+
+    @Test
+    @DisplayName("회원가입 - 실패(이미 존재하는 사용자)")
+    void signUp_fail_() {
+        UserSignUpRequest param = UserSignUpRequest.builder()
+                .nickName("test")
+                .password("test1234")
+                .email("email@email.com")
+                .country("SOUTH_KOREA")
+                .interestCountry("SOUTH_KOREA")
+                .region("Seoul")
+                .profileImage("image")
+                .build();
+
+        when(userRepository.findByEmail(anyString()))
+                .thenReturn(Optional.of(new User()));
+
+        assertThatThrownBy(() -> userSignUpService.signUp(param))
+                .isInstanceOf(CustomException.class)
+                .hasMessage(EXISTING_USER.getMessage());
+    }
+}
---
+++ src/test/java/com/backend/immilog/user/presentation/controller/UserControllerTest.java
@@ -0,0 +1,69 @@
+package com.backend.immilog.user.presentation.controller;
+
+import com.backend.immilog.global.presentation.response.ApiResponse;
+import com.backend.immilog.user.application.EmailService;
+import com.backend.immilog.user.application.UserSignUpService;
+import com.backend.immilog.user.enums.EmailComponents;
+import com.backend.immilog.user.presentation.request.UserSignUpRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.util.Pair;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+import static org.springframework.http.HttpStatus.CREATED;
+
+@DisplayName("사용자 컨트롤러 테스트")
+class UserControllerTest {
+    @Mock
+    private UserSignUpService userSignUpService;
+    @Mock
+    private EmailService emailService;
+    private UserController userController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        userController = new UserController(userSignUpService, emailService);
+    }
+
+    @Test
+    @DisplayName("회원가입")
+    void signUp() {
+        // given
+        UserSignUpRequest param = UserSignUpRequest.builder()
+                .nickName("test")
+                .password("test1234")
+                .email("email@email.com")
+                .country("SOUTH_KOREA")
+                .interestCountry("SOUTH_KOREA")
+                .region("Seoul")
+                .profileImage("image")
+                .build();
+
+        when(userSignUpService.signUp(param))
+                .thenReturn(Pair.of(1L, "test"));
+
+        // when
+        ResponseEntity<ApiResponse> response = userController.signUp(param);
+
+        // then
+        verify(emailService, times(1)).sendHtmlEmail(
+                param.getEmail(),
+                EmailComponents.EMAIL_SIGN_UP_SUBJECT,
+                String.format(
+                        EmailComponents.HTML_SIGN_UP_CONTENT,
+                        "test",
+                        String.format(
+                                EmailComponents.API_LINK,
+                                1L
+                        )
+                )
+        );
+        assertThat(response.getStatusCode()).isEqualTo(CREATED);
+    }
+}
