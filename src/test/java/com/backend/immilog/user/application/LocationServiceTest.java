d18e2ef6e15322af8be588501710a8c078d7cb0a
HyunsooZo <bzhs1992@icloud.com>
2024-08-14 21:10:46 +0900

    test (location) : 좌표를 이용해 국가 정보 가져오는 기능 테스트코드 추가

---
+++ src/test/java/com/backend/immilog/user/application/LocationServiceTest.java
@@ -0,0 +1,117 @@
+package com.backend.immilog.user.application;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.util.Pair;
+import org.springframework.http.MediaType;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.CompletableFuture;
+
+import static com.backend.immilog.user.enums.Countries.SOUTH_KOREA;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withServerError;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+
+@DisplayName("위치 서비스 테스트")
+class LocationServiceTest {
+    private LocationService locationService;
+    private MockRestServiceServer mockServer;
+
+    @BeforeEach
+    void setUp() {
+        RestTemplate restTemplate = new RestTemplate();
+        locationService = new LocationService(restTemplate);
+        mockServer = MockRestServiceServer.bindTo(restTemplate).build();
+        ReflectionTestUtils.setField(locationService, "geocoderUrl", "https://example.com/geocode?lat=%f&lng=%f&key=%s");
+        ReflectionTestUtils.setField(locationService, "geocoderKey", "fake-key");
+    }
+
+    @Test
+    @DisplayName("API 호출 성공")
+    void getCountry_success() {
+        // given
+        Double latitude = 37.5665;
+        Double longitude = 126.9780;
+
+        // Mock 서버에서 반환할 올바른 JSON 응답 설정
+        mockServer.expect(MockRestRequestMatchers.anything())
+                .andRespond(withSuccess("""
+                    {
+                    "plus_code": {
+                        "compound_code": "HX8H+H6R 대한민국 서울특별시",
+                        "global_code": "8Q98HX8H+H6R"
+                    }
+                    }""".trim(), MediaType.APPLICATION_JSON));
+
+        // when
+        CompletableFuture<Pair<String, String>> resultFuture =
+                locationService.getCountry(latitude, longitude);
+        Pair<String, String> result = resultFuture.join();
+
+        // then
+        assertThat(result.getFirst()).isEqualTo(SOUTH_KOREA.getCountryName());
+        assertThat(result.getSecond()).isEqualTo("서울특별시");
+    }
+
+    @Test
+    @DisplayName("API 호출 실패 - 서버 에러")
+    void getCountry_fail_apiException() {
+        // given
+        Double latitude = 37.5665;
+        Double longitude = 126.9780;
+
+        // Mock 서버에서 에러 응답 설정
+        mockServer.expect(MockRestRequestMatchers.anything())
+                .andRespond(withServerError());
+
+        // when
+        CompletableFuture<Pair<String, String>> resultFuture =
+                locationService.getCountry(latitude, longitude);
+        Pair<String, String> result = resultFuture.join();
+
+        // then
+        assertThat(result.getFirst()).isEqualTo("기타 국가");
+        assertThat(result.getSecond()).isEqualTo("기타 지역");
+    }
+
+    @Test
+    @DisplayName("API 호출 실패 - 잘못된 좌표")
+    void getCountry_fail_invalidCoordinates() {
+        // given
+        Double latitude = -1.0;
+        Double longitude = -1.0;
+
+        // when
+        CompletableFuture<Pair<String, String>> resultFuture = locationService.getCountry(latitude, longitude);
+        Pair<String, String> result = resultFuture.join();
+
+        // then
+        assertThat(result).isNull();
+    }
+
+    @Test
+    @DisplayName("API 호출 실패 - json 파싱 실패")
+    void getCountry_fail_json_invalid() {
+        // given
+        Double latitude = 37.5665;
+        Double longitude = 126.9780;
+
+        // Mock 서버에서 반환할 올바른 JSON 응답 설정
+        mockServer.expect(MockRestRequestMatchers.anything())
+                .andRespond(withSuccess("""
+                        }{
+                        """.trim(), MediaType.APPLICATION_JSON));
+
+        // when
+        CompletableFuture<Pair<String, String>> resultFuture =
+                locationService.getCountry(latitude, longitude);
+
+        // then
+        assertThat(resultFuture.join().getFirst()).isEqualTo("기타 국가");
+    }
+}
