6e88702d433f174dac4284996f11f3f5b42bfd79
HyunsooZo <bzhs1992@icloud.com>
2024-08-23 21:08:41 +0900

    refactor (post) : DTO 클래스를 Java 레코드로 변경하여 불변성과 간결성 향상

--- src/main/java/com/backend/immilog/post/presentation/request/PostUploadRequest.java
+++ src/main/java/com/backend/immilog/post/presentation/request/PostUploadRequest.java
@@ -5,30 +5,19 @@ import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
 @ApiModel(value = "PostUploadRequest", description = "게시물 생성 요청 DTO")
-public class PostUploadRequest {
-    @NotBlank(message = "제목을 입력해주세요.")
-    private String title;
-
-    @NotBlank(message = "내용을 입력해주세요.")
-    private String content;
-
-    private List<String> tags;
-
-    private List<String> attachments;
-
-    @NotNull(message = "전체공개 여부를 입력해주세요.")
-    private Boolean isPublic;
-
-    private Categories category;
+public record PostUploadRequest(
+        @NotBlank(message = "제목을 입력해주세요.") String title,
+        @NotBlank(message = "내용을 입력해주세요.") String content,
+        List<String> tags,
+        List<String> attachments,
+        @NotNull(message = "전체공개 여부를 입력해주세요.") Boolean isPublic,
+        @NotNull(message = "카테고리를 입력해주세요.")Categories category
+) {
 }
