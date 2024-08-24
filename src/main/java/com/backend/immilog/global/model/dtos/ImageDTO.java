5301309e962ceb26359a376d5e9edac1b6671efa
HyunsooZo <bzhs1992@icloud.com>
2024-08-25 03:43:18 +0900

    refactor (global) : DTO 클래스를 Java 레코드로 변경하여 불변성과 간결성 향상

--- src/main/java/com/backend/immilog/global/model/dtos/ImageDTO.java
+++ src/main/java/com/backend/immilog/global/model/dtos/ImageDTO.java
@@ -1,20 +1,13 @@
 package com.backend.immilog.global.model.dtos;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
-public class ImageDTO {
-
-    private List<String> imageUrl;
-
+public record ImageDTO(
+        List<String> imageUrl
+) {
     public static ImageDTO from(
             List<String> imageUrl
     ) {
