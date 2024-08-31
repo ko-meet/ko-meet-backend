6050f215a90c243a52c2c554bcde967e867b69d8
HyunsooZo <bzhs1992@icloud.com>
2024-08-31 23:52:14 +0900

    feat (global) : Swagger 에서 Token 받는 방식 변경
    - `@ExtractUserId` 어노테이션을 통한 검증으로 인해
       Swagger 에서 헤더를 강제하지 않으므로 설정을 수정하여
       선택적으로 입력할 수 있도록 수정

--- src/main/java/com/backend/immilog/global/security/UserIdExtractionAspect.java
+++ src/main/java/com/backend/immilog/global/security/UserIdExtractionAspect.java
@@ -2,12 +2,13 @@ package com.backend.immilog.global.security;
 
 import com.backend.immilog.global.enums.UserRole;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+
+import static org.apache.http.HttpHeaders.AUTHORIZATION;
 
 @Aspect
 @Component
@@ -18,19 +19,16 @@ public class UserIdExtractionAspect {
     private final HttpServletRequest request;
 
     @Before("@annotation(com.backend.immilog.global.security.ExtractUserId)")
-    public void extractUserId(JoinPoint joinPoint) {
-        String authorizationHeader = request.getHeader("Authorization");
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            Long userSeq = tokenProvider.getIdFromToken(authorizationHeader);
-            request.setAttribute("userSeq", userSeq);
-        }
+    public void extractUserId() {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        Long userSeq = tokenProvider.getIdFromToken(authorizationHeader);
+        request.setAttribute("userSeq", userSeq);
     }
+
     @Before("@annotation(com.backend.immilog.global.security.ExtractUserId)")
-    public void extractUserRole(JoinPoint joinPoint) {
-        String authorizationHeader = request.getHeader("Authorization");
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            UserRole userRole = tokenProvider.getUserRoleFromToken(authorizationHeader);
-            request.setAttribute("userRole", userRole);
-        }
+    public void extractUserRole() {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        UserRole userRole = tokenProvider.getUserRoleFromToken(authorizationHeader);
+        request.setAttribute("userRole", userRole);
     }
 }
