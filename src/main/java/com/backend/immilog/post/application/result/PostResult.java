ac1d7dc645f48839158a8e7f522074be7885fbaf
HyunsooZo <bzhs1992@icloud.com>
2024-09-07 20:27:23 +0900

    feat (post) : 계층 간 DTO 분리로 레이어 간 의존성 최소화 (#260)
    * rename (post) : User 도메인 수정으로 인한 객체명 및 임포트 경로 수정
    
    * feat (post) : 계층 간 DTO 분리
    
    - 인프라 레이어에서 직접 엔티티를 받는 DTO 추가
    - application 레이어에서 사용할 서비스 DTO로 변환하는 로직 추가
    - 각 레이어 간 의존성을 최소화하여 계층 간 결합도 감소
    
    * test (post) : 계층 간 DTO 분리로 인한 테스트코드 수정

--- src/main/java/com/backend/immilog/post/application/result/PostResult.java
+++ src/main/java/com/backend/immilog/post/application/result/PostResult.java
@@ -1,111 +1,59 @@
 package com.backend.immilog.post.application.result;
 
-import com.backend.immilog.post.domain.vo.PostMetaData;
-import com.backend.immilog.post.domain.vo.PostUserData;
-import com.backend.immilog.post.domain.model.InteractionUser;
-import com.backend.immilog.post.domain.model.Post;
-import com.backend.immilog.post.domain.model.PostResource;
 import com.backend.immilog.post.domain.enums.Categories;
-import com.backend.immilog.post.domain.enums.InteractionType;
 import com.backend.immilog.post.domain.enums.PostStatus;
-import com.backend.immilog.post.domain.enums.ResourceType;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Builder;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-import static com.backend.immilog.post.domain.enums.InteractionType.BOOKMARK;
-import static com.backend.immilog.post.domain.enums.InteractionType.LIKE;
-import static com.backend.immilog.post.domain.enums.ResourceType.ATTACHMENT;
-import static com.backend.immilog.post.domain.enums.ResourceType.TAG;
-
-@Getter
-@Setter
-@ToString
-public class PostResult {
-    private Long seq;
-    private String title;
-    private String content;
-    private Long userSeq;
-    private String userProfileUrl;
-    private String userNickName;
-    private List<CommentResult> comments;
-    private Long commentCount;
-    private Long viewCount;
-    private Long likeCount;
-    private List<String> tags;
-    private List<String> attachments;
-    private List<Long> likeUsers;
-    private List<Long> bookmarkUsers;
-    private String isPublic;
-    private String country;
-    private String region;
-    private Categories category;
-    private PostStatus status;
-    private String createdAt;
-
-    public PostResult(
-            Post post,
-            List<InteractionUser> interactionUsers,
-            List<PostResource> postResources
+@Builder
+public record PostResult(
+        Long seq,
+        String title,
+        String content,
+        Long userSeq,
+        String userProfileUrl,
+        String userNickName,
+        List<CommentResult> comments,
+        Long commentCount,
+        Long viewCount,
+        Long likeCount,
+        List<String> tags,
+        List<String> attachments,
+        List<Long> likeUsers,
+        List<Long> bookmarkUsers,
+        String isPublic,
+        String country,
+        String region,
+        Categories category,
+        PostStatus status,
+        String createdAt
+) {
+    public PostResult copyWithNewComments(
+            List<CommentResult> comments
     ) {
-        interactionUsers = interactionUsers != null ? interactionUsers : List.of();
-        postResources = postResources != null ? postResources : List.of();
-        PostMetaData postMetaData = post.postMetaData();
-        PostUserData postUserData = post.postUserData();
+        return PostResult.builder()
+                .seq(this.seq())
+                .title(this.title())
+                .content(this.content())
+                .userSeq(this.userSeq())
+                .userProfileUrl(this.userProfileUrl())
+                .userNickName(this.userNickName())
+                .comments(comments)
+                .commentCount(this.commentCount())
+                .viewCount(this.viewCount())
+                .likeCount(this.likeCount())
+                .tags(this.tags())
+                .attachments(this.attachments())
+                .likeUsers(this.likeUsers())
+                .bookmarkUsers(this.bookmarkUsers())
+                .isPublic(this.isPublic())
+                .country(this.country())
+                .region(this.region())
+                .category(this.category())
+                .status(this.status())
+                .createdAt(this.createdAt())
+                .build();
 
-        List<Long> likeUsers = getLongs(interactionUsers, post, LIKE);
-        List<Long> bookmarkUsers = getLongs(interactionUsers, post, BOOKMARK);
-        List<String> tags = getStrings(postResources, post, TAG);
-        List<String> attachments = getStrings(postResources, post, ATTACHMENT);
-
-        this.seq = post.seq();
-        this.title = postMetaData.getTitle();
-        this.content = postMetaData.getContent();
-        this.userSeq = postUserData.getUserSeq();
-        this.userProfileUrl = postUserData.getProfileImage();
-        this.userNickName = postUserData.getNickname();
-        this.country = postMetaData.getCountry().getCountryName();
-        this.region = postMetaData.getRegion();
-        this.viewCount = postMetaData.getViewCount();
-        this.likeCount = postMetaData.getLikeCount();
-        this.commentCount = post.commentCount();
-        this.comments = new ArrayList<>();
-        this.likeUsers = likeUsers;
-        this.bookmarkUsers = bookmarkUsers;
-        this.tags = tags;
-        this.attachments = attachments;
-        this.isPublic = post.isPublic();
-        this.status = postMetaData.getStatus();
-        this.category = post.category();
-        this.createdAt = post.createdAt().toString();
     }
-
-    private static List<String> getStrings(
-            List<PostResource> postResources,
-            Post post,
-            ResourceType type
-    ) {
-        return postResources.stream()
-                .filter(Objects::nonNull)
-                .filter(c -> c.resourceType().equals(type))
-                .map(PostResource::content)
-                .toList();
-    }
-
-    private static List<Long> getLongs(
-            List<InteractionUser> interactionUsers,
-            Post post,
-            InteractionType type
-    ) {
-        return interactionUsers.stream()
-                .filter(Objects::nonNull)
-                .filter(c -> c.interactionType().equals(type))
-                .map(InteractionUser::userSeq)
-                .toList();
-    }
-
 }
