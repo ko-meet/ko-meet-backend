27e820c4502db54ab01cf0d965da700f14cb7cbd
HyunsooZo <bzhs1992@icloud.com>
2024-09-08 16:46:56 +0900

    test (user) : 사용자 회사 정보 등록 테스트 코드 추가

---
+++ src/test/java/com/backend/immilog/user/presentation/controller/CompanyControllerTest.java
@@ -0,0 +1,59 @@
+package com.backend.immilog.user.presentation.controller;
+
+import com.backend.immilog.user.application.CompanyRegisterService;
+import com.backend.immilog.user.application.command.CompanyRegisterCommand;
+import com.backend.immilog.user.domain.model.enums.Industry;
+import com.backend.immilog.user.presentation.request.CompanyRegisterRequest;
+import com.backend.immilog.user.presentation.response.UserApiResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static com.backend.immilog.user.domain.model.enums.UserCountry.SOUTH_KOREA;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+@DisplayName("CompanyController 테스트")
+class CompanyControllerTest {
+    @Mock
+    private CompanyRegisterService companyRegisterService;
+    private CompanyController companyController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        companyController = new CompanyController(companyRegisterService);
+    }
+
+    @Test
+    @DisplayName("회사정보 등록 - 성공")
+    void registerCompany() {
+        // given
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        CompanyRegisterRequest param = CompanyRegisterRequest.builder()
+                .industry(Industry.IT)
+                .companyName("회사명")
+                .companyEmail("email@email.com")
+                .companyPhone("010-1234-5678")
+                .companyAddress("주소")
+                .companyHomepage("홈페이지")
+                .companyCountry(SOUTH_KOREA)
+                .companyRegion("지역")
+                .companyLogo("로고")
+                .build();
+        CompanyRegisterCommand command = param.toCommand();
+        when(request.getAttribute("userSeq")).thenReturn(1L);
+        // when
+        ResponseEntity<UserApiResponse> response = companyController.registerCompany(request, param);
+        // then
+        verify(companyRegisterService).registerOrEditCompany(anyLong(), any());
+    }
+}
