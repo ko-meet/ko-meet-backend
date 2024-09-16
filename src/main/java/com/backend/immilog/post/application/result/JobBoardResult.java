e5b7f86b2d29334e1fd54444866fb04e8fc4f8e4
HyunsooZo <bzhs1992@icloud.com>
2024-09-16 17:15:11 +0900

    feat (post) : 구인구직 게시글 조건 별 조회 기능 추가 (#277)
    * feat (post) : 구인구직 게시글 조건 별 조회 기능 추가
    
    * test (post) : 구인구직 게시글 조건 별 조회 테스트코드 추가

---
+++ src/main/java/com/backend/immilog/post/application/result/JobBoardResult.java
@@ -0,0 +1,39 @@
+package com.backend.immilog.post.application.result;
+
+import com.backend.immilog.post.domain.model.enums.Countries;
+import com.backend.immilog.post.domain.model.enums.Experience;
+import com.backend.immilog.post.domain.model.enums.PostStatus;
+import com.backend.immilog.user.domain.model.enums.Industry;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+public record JobBoardResult(
+        Long seq,
+        String title,
+        String content,
+        Long viewCount,
+        Long likeCount,
+        List<String> tags,
+        List<String> attachments,
+        List<Long> likeUsers,
+        List<Long> bookmarkUsers,
+        Countries country,
+        String region,
+        Industry industry,
+        LocalDateTime deadline,
+        Experience experience,
+        String salary,
+        String companyName,
+        String companyEmail,
+        String companyPhone,
+        String companyAddress,
+        String companyHomepage,
+        String companyLogo,
+        Long companyManagerUserSeq,
+        PostStatus status,
+        LocalDateTime createdAt
+) {
+}
