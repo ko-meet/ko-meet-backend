1f32eae1e2b3b1c960797ab00670c2fdbbaa8b96
HyunsooZo <bzhs1992@icloud.com>
2024-09-17 18:55:35 +0900

    feat (post) : 게시물 검색 시 키워드에 따른 검색 결과부분 하이라이트 노출 되도록 수정 (#279)

--- src/main/java/com/backend/immilog/post/application/result/PostResult.java
+++ src/main/java/com/backend/immilog/post/application/result/PostResult.java
@@ -2,11 +2,16 @@ package com.backend.immilog.post.application.result;
 
 import com.backend.immilog.post.domain.model.enums.Categories;
 import com.backend.immilog.post.domain.model.enums.PostStatus;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record PostResult(
         Long seq,
         String title,
@@ -27,7 +32,8 @@ public record PostResult(
         String region,
         Categories category,
         PostStatus status,
-        String createdAt
+        String createdAt,
+        String keyword
 ) {
     public PostResult copyWithNewComments(
             List<CommentResult> comments
@@ -53,7 +59,89 @@ public record PostResult(
                 .category(this.category())
                 .status(this.status())
                 .createdAt(this.createdAt())
+                .keyword(null)
                 .build();
-
     }
+
+    public PostResult copyWithKeyword(
+            String keyword
+    ) {
+        String contentResult = extractKeyword(this.content, keyword, 50, 5);
+        String titleResult = extractKeyword(this.title(), keyword, 20, 5);
+        List<String> tagResult = extractTags(this.tags(), keyword);
+        return PostResult.builder()
+                .seq(this.seq())
+                .title(titleResult)
+                .content(contentResult)
+                .userSeq(this.userSeq())
+                .userProfileUrl(this.userProfileUrl())
+                .userNickName(this.userNickName())
+                .comments(this.comments())
+                .commentCount(this.commentCount())
+                .viewCount(this.viewCount())
+                .likeCount(this.likeCount())
+                .tags(tagResult)
+                .attachments(this.attachments())
+                .likeUsers(this.likeUsers())
+                .bookmarkUsers(this.bookmarkUsers())
+                .isPublic(this.isPublic())
+                .country(this.country())
+                .region(this.region())
+                .category(this.category())
+                .status(this.status())
+                .createdAt(this.createdAt())
+                .keyword(keyword)
+                .build();
+    }
+
+    private static String extractKeyword(
+            String text,
+            String keyword,
+            int after,
+            int before
+    ) {
+
+        int keywordIndex = text.indexOf(keyword);
+        if (keywordIndex != -1) {
+            int start = Math.max(keywordIndex - before, 0);
+            int end = Math.min(keywordIndex + keyword.length() + after, text.length());
+            return text.substring(start, end);
+        } else {
+            return text.substring(0, Math.min(text.length(), after));
+        }
+    }
+
+    private static List<String> extractTags(
+            List<String> tags,
+            String keyword
+    ) {
+        if (tags.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        List<String> keywordTagList = tags.stream()
+                .filter(tag -> tag.contains(keyword))
+                .limit(1)
+                .toList();
+
+        boolean isAdded = !keywordTagList.isEmpty();
+
+        List<String> shuffledTags = tags.stream()
+                .filter(tag -> !keywordTagList.contains(tag)) // 이미 추가된 태그는 제외
+                .collect(Collectors.collectingAndThen(
+                        Collectors.toList(), collected -> {
+                            Collections.shuffle(collected);
+                            return collected.stream()
+                                    .limit(isAdded ?
+                                            Math.min(2, collected.size()) :
+                                            Math.min(3, collected.size())
+                                    ).toList();
+                        }
+                ));
+
+        List<String> result = new ArrayList<>(keywordTagList);
+        result.addAll(shuffledTags);
+        return result;
+    }
+
 }
