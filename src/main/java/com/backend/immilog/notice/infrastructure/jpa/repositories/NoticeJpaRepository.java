8f9e4dc3e7ea8f8ad622e55dbd5d52013ddff81b
HyunsooZo <bzhs1992@icloud.com>
2024-10-08 21:01:00 +0700

    feat (notice) : 사용자가 읽지 않은 공지사항 존재여부 체크 api 추가 (#297)
    * feat (notice) : 사용자가 읽지 않은 공지사항 존재여부 체크 api 추가
    
    * test (notice) : 공지사항 존재여부 체크 테스트코드 추가

--- src/main/java/com/backend/immilog/notice/infrastructure/jpa/repositories/NoticeJpaRepository.java
+++ src/main/java/com/backend/immilog/notice/infrastructure/jpa/repositories/NoticeJpaRepository.java
@@ -1,7 +1,12 @@
 package com.backend.immilog.notice.infrastructure.jpa.repositories;
 
+import com.backend.immilog.notice.domain.model.enums.NoticeCountry;
 import com.backend.immilog.notice.infrastructure.jpa.entities.NoticeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoticeJpaRepository extends JpaRepository<NoticeEntity, Long> {
+    Boolean existsByTargetCountriesContainingAndReadUsersNotContaining(
+            NoticeCountry country,
+            Long seq
+    );
 }
